b97395b74e0ec976f56e9c06eb36c6d08ff03da6
ThePlatinumDragon999 <flailsnail999@gmail.com>
2026-08-19 21:50:13 -0600

    Time: 0 ms (100%), Space: 19.7 MB (8.56%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -14,31 +14,28 @@ public:
         ListNode dummy(0);
         ListNode* iter = &dummy;
 
-        ListNode* p1 = list1;
-        ListNode* p2 = list2;
-
-        while (p1 && p2) 
+        while (list1 && list2) 
         {
-            if (p1->val < p2->val) 
+            if (list1->val < list2->val) 
             {
-                iter->next = p1;
-                p1 = p1->next;
+                iter->next = list1;
+                list1 = list1->next;
             }
             else
             {
-                iter->next = p2;
-                p2 = p2->next;
+                iter->next = list2;
+                list2 = list2->next;
             }
 
             iter = iter->next;
         }
         
         // Dealing with leftover nodes
-        if (p1) {
-            iter->next = p1;
+        if (list1) {
+            iter->next = list1;
         }
-        if (p2) {
-            iter->next = p2;
+        if (list2) {
+            iter->next = list2;
         }
         
         return dummy.next;
