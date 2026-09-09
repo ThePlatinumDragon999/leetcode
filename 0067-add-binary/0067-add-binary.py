4996902c5ccbeca42dd7c7bc0b2e95788d7259fb
ThePlatinumDragon999 <flailsnail999@gmail.com>
2026-09-08 22:32:45 -0600

    Time: 2 ms (48.42%), Space: 19.2 MB (85.08%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a) - 1
+        j = len(b) - 1
+        cin = 0
+        result = ""
+
+        while (i >= 0 or j >= 0 or cin):
+            bitA = int(a[i]) if i >= 0 else 0
+            bitB = int(b[j]) if j >= 0 else 0
+            sum = bitA ^ bitB ^ cin
+            cin = (bitA & bitB) | (cin & (bitA ^ bitB))
+            result += str(sum)
+            i -= 1
+            j -= 1
+        
+        return ''.join(reversed(result))
+        
