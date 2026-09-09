196469fabc633d0f83cf91fe12b54f04109af03d
ThePlatinumDragon999 <flailsnail999@gmail.com>
2026-09-08 22:33:40 -0600

    Time: 1 ms (54.29%), Space: 19.4 MB (51.6%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -3,14 +3,14 @@ class Solution:
         i = len(a) - 1
         j = len(b) - 1
         cin = 0
-        result = ""
+        result = []
 
         while (i >= 0 or j >= 0 or cin):
             bitA = int(a[i]) if i >= 0 else 0
             bitB = int(b[j]) if j >= 0 else 0
             sum = bitA ^ bitB ^ cin
             cin = (bitA & bitB) | (cin & (bitA ^ bitB))
-            result += str(sum)
+            result.append(str(sum))
             i -= 1
             j -= 1
         
