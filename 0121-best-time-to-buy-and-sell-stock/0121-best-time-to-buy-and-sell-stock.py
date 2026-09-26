53e36a6840b6871a18d94889a464dfc4f8e247a2
ThePlatinumDragon999 <flailsnail999@gmail.com>
2026-09-26 12:25:26 -0600

    Time: 78 ms (9.33%), Space: 28.5 MB (77.27%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         lowestPriceSoFar = prices[0]
-        currentProfit = 0
         maxProfit = 0
 
         for i in range(len(prices) - 1):
@@ -9,7 +8,7 @@ class Solution:
             currentProfit = currentPrice - lowestPriceSoFar
             lowestPriceSoFar = min(lowestPriceSoFar, currentPrice)
 
-            if (currentProfit > maxProfit):
+            if (currentPrice - lowestPriceSoFar > maxProfit):
                 maxProfit = currentProfit
         
         return maxProfit
